8408cc66c0c467d58a4fbbe047d2f484fea9fed0
Thanos Tsoukalas <tsouthanos@gmail.com>
2025-08-14 19:02:50 -0700

    βελτίωση του main script

--- src/trigonator.cpp
+++ src/trigonator.cpp
@@ -1,62 +1,39 @@
+#include "types.h"
 #include "calculator.h"
+#include "interface.h"
+
 #include <iostream>
 #include <cstdlib>
-/*
-    Σκέφτομαι να βάλω και άλλες δυνατότητες (όπως υπολογισμό τετραγώνου και κύκλου),
-    αλλά το πρόγραμμα θα το αποκαλώ για πάντα «Τριγωνέιτορ»
-*/
+
 int main (){
     #ifdef _WIN32
-        system("chcp 65001");
-        system("cls");
-    #elif __linux__
-        system("clear");
+        system("title Trigonator v"APP_VER);
     #endif
+    SetGreekCharScheme();
+
+    std::cout << "Τριγωνέιτορ v" << APP_VER << std::endl << std::endl;
 
     while (true)
     {   
-        int option;
-        std::cout << "Τριγωνέιτορ v1.2" << std::endl << std::endl; 
-        std::cout << "Επιλέξτε σχήμα: " << std::endl;
-        std::cout << "[1] Τρίγωνο" << std::endl;
-        std::cout << "[2] Τετράγωνο" << std::endl;
-        std::cout << "[3] Έξοδος" << std::endl << std::endl;
-        std::cout << "Επιλογή: ";
-        std::cin >> option;
+        std::string option = DisplayAndGetOption({
+            "Ανάλυση Τριγώνου",
+            "Ανάλυση Τετραγώνου",
+            "Εκκαθάριση Τερματικού",
+            "Σχετικά με το Τριγωνέιτορ",
+            "Έξοδος"
+        });
 
-        if (option == 1)
+        if (option == "Ανάλυση Τριγώνου")
         {
-            vertex a;
-            vertex b;
-            vertex c;
-            std::cout << "Στοιχεία Τριγώνου" << std::endl << std::endl;
-            std::cout << "Σημείο Α(x, y):" << std::endl;
-            std::cout << "x: ";
-            std::cin >> a.x;
-            std::cout << std::endl << "y: ";
-            std::cin >> a.y;
-            std::cout << std::endl << std::endl;
-            std::cout << "Σημείο Β(x1, y1):" << std::endl;
-            std::cout << "x1: ";
-            std::cin >> b.x;
-            std::cout << std::endl << "y1: ";
-            std::cin >> b.y;
-            std::cout << std::endl << std::endl;
-            std::cout << "Σημείο Γ(x2, y2):" << std::endl;
-            std::cout << "x2: ";
-            std::cin >> c.x;
-            std::cout << std::endl << "y2: ";
-            std::cin >> c.y;
-            std::cout << std::endl << std::endl;
-
-            triangle usr_triangle = {a, b, c};
-
-            DisplayTriangleInfo(usr_triangle);
+            Triangle triangle_to_analyze = GetTriangleVertices();
+            TriangleProperties triangle_props = CalculateTriangleProperties(&triangle_to_analyze);
+            std::cout << "Returned properties" << std::endl;
+            DisplayTriangleInfo(triangle_props);
         }
-        else if (option == 2)
+        else if (option == "Ανάλυση Τετραγώνου")
         {
-            vertex a;
-            vertex b;
+            Point a;
+            Point b;
 
             std::cout << "Στοιχεία Τετραγώνου" << std::endl << std::endl;
             std::cout << "Σημείο Α(x, y): (ένα άκρο της διαγωνίου) " << std::endl << std::endl;
@@ -70,21 +47,29 @@ int main (){
             std::cout << "y1: ";
             std::cin >> b.y;
 
-            rectangle usr_rectangle = {a, b};
+            Rectangle usr_rectangle = {a, b};
 
             DisplayRectInfo(usr_rectangle);
         }
-        else if (option == 3)
+        else if (option == "Έξοδος")
         {
             break;
             return EXIT_SUCCESS;
         }
+        else if (option == "Εκκαθάριση Τερματικού")
+        {
+            ClearTerminal();
+        }
+        else if (option == "Σχετικά με το Τριγωνέιτορ")
+        {
+            DisplayApplicationInfo();
+        }
         else
         {
             std::cout << "Δεν υπάρχει εντολή με ταυτοποιητή [" << option << "]. Δοκιμάστε ξανά." << std::endl;
-            break;
         }
         
     }
+
     return 0;
 }
